eda80f2bdc9f12afb759a5c0587caf29c5d1b05e
josegrd <josegarda8@gmail.com>
2024-09-22 12:16:39 +0700

    edit cross origin

--- src/main/java/com/josegrd/web_self_api/controller/BlogController.java
+++ src/main/java/com/josegrd/web_self_api/controller/BlogController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@CrossOrigin(origins = "https://gardio.vercel.app", allowCredentials = "true")
+@CrossOrigin(origins = {"https://gardio.vercel.app", "http://localhost:5173"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/api/v1/blog")
 @RequiredArgsConstructor
